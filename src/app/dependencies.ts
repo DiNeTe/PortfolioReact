5ffc678199804a55f53356d5374a5f998da24139
Pierre.WEBER <wbrp51@gmail.com>
2024-06-16 15:21:17 +0200

    improved html meta tag

--- src/app/dependencies.ts
+++ src/app/dependencies.ts
@@ -1,5 +1,6 @@
 import { ProjectDataSource } from "../data/ProjectDataSource";
 
-// agit comme un contrat pour les dépendances, indique quelle dépendance et de quel type doit être fourni 
+// agit comme un contrat pour les dépendances, indique quelle dépendance et de quel type doit être fourni
 export type AppDependencies = {
-  projectDataSource: ProjectDataSource;}
+  projectDataSource: ProjectDataSource;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,25 @@
-import "./main.css"
+import "./main.css";
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import { AppDependencies } from "./app/dependencies.ts";
 import fakeProjectDataSource from "./data/fakeProjectDataSource.tsx";
 import { AppDependenciesContext } from "./app/context.ts";
 
-
-import App from './App.tsx'
+import App from "./App.tsx";
 
 const appDependencies: AppDependencies = {
   projectDataSource: fakeProjectDataSource,
 };
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AppDependenciesContext.Provider value={appDependencies}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </AppDependenciesContext.Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
