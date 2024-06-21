453f1acff49082e6bc530444f8fc84ae46d9cca5
Pierre.WEBER <wbrp51@gmail.com>
2024-06-21 11:26:55 +0200

    install vercel analytic and speed Insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import AppRouter from "./routes/AppRouter";
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { inject } from '@vercel/analytics';
+
+// Inject the Vercel Analytics script
+inject();
 
 const App: React.FC = () => {
   return (
     <div className="app">
       <main>
         <AppRouter />
+        <SpeedInsights />
       </main>
     </div>
   );
