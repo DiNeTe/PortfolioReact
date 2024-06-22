b436422f424ce0220632026486e43a3a1837b207
Pierre.WEBER <wbrp51@gmail.com>
2024-06-22 15:19:32 +0200

    change all favicon

--- src/components/WindowSkills.tsx
+++ src/components/WindowSkills.tsx
@@ -24,37 +24,37 @@ const SkillsContent: React.FC = () => {
 
   // Effet pour gérer l'affichage des compétences au défilement
   useEffect(() => {
-    const skillsContainer = document.getElementById('skills-container');
+    const skillsContainer = document.getElementById("skills-container");
 
     const handleScroll = () => {
-      const skillContents = document.querySelectorAll('.skill-content');
+      const skillContents = document.querySelectorAll(".skill-content");
       skillContents.forEach((content) => {
         // Obtient la position de chaque élément par rapport au haut de la fenêtre
         const contentTop = content.getBoundingClientRect().top;
         const windowHeight = window.innerHeight;
 
-        if (contentTop < windowHeight -150 && contentTop > -90) {
-          content.classList.add('visible');
+        if (contentTop < windowHeight * 0.6 && contentTop > -10) {
+          content.classList.add("visible");
         } else {
-          content.classList.remove('visible');
+          content.classList.remove("visible");
         }
       });
     };
 
     if (skillsContainer) {
-        // Ajoute l'écouteur d'événements 'scroll' pour appeler handleScroll lors du défilement
-      skillsContainer.addEventListener('scroll', handleScroll);
+      // Ajoute l'écouteur d'événements 'scroll' pour appeler handleScroll lors du défilement
+      skillsContainer.addEventListener("scroll", handleScroll);
       console.log("Scroll event listener added");
 
       // Déclenche l'événement 'scroll' initialement après un court délai pour s'assurer que tous les éléments sont montés
       setTimeout(() => {
         handleScroll();
-        skillsContainer.dispatchEvent(new Event('scroll'));
+        skillsContainer.dispatchEvent(new Event("scroll"));
       }, 100);
 
-       // Fonction de nettoyage pour retirer l'écouteur d'événements lors du démontage du composant ou du changement de 'skills'
+      // Fonction de nettoyage pour retirer l'écouteur d'événements lors du démontage du composant ou du changement de 'skills'
       return () => {
-        skillsContainer.removeEventListener('scroll', handleScroll);
+        skillsContainer.removeEventListener("scroll", handleScroll);
         console.log("Scroll event listener removed");
       };
     }
